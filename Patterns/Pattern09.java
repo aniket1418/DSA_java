bf1d621aea28ca027cb1ff6cb8ac335384749fc4
am094137 <aniket.mehta@cerner.com>
2023-01-06 18:25:59 +0530

    patterns problems

---
+++ Patterns/Pattern09.java
@@ -0,0 +1,29 @@
+package Patterns;
+
+/*
+ * Problem - patter
+ * 
+ *      * * * * *
+ *    * * * * *
+ *  * * * * *
+ * * * * * *
+ */   
+
+public class Pattern09 {
+    
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<=n; i++){
+            //spaces
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+
+            for (int j=1; j<=5; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
